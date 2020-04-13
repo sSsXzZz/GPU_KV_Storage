0b063d34870c681b7389bd17f7f474ae3907b5c1
Shalin Patel <shalinpatel75@gmail.com>
2020-04-12 22:30:57 -0400

    Created C++ style implementation of basic hash table

--- src/hash.cu
+++ src/hash.cu
@@ -1,30 +1,11 @@
 #include "hash.h"
-#include <cstdio>
-#include <cstddef>
 
-// function to add the elements of two arrays
-/*
-__global__
-void add(int n, float *x, float *y)
-{
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    int stride = blockDim.x * gridDim.x;
-    for (int i = index; i < n; i += stride) {
-        y[i] = x[i] + y[i];
-    }
-}
-*/
-
-__device__
-void init_hash_entry(hash_entry_t *entry)
-{
-    memset(entry, 0, sizeof(hash_entry_t));
-    entry->occupied = false; // redundant but just to be certain
+__device__ void init_hash_entry(hash_entry_t* entry) {
+    std::memset(entry, 0, sizeof(hash_entry_t));
+    entry->occupied = false;  // redundant but just to be certain
 }
 
-__global__
-void init_hash_table(int n, hash_table_t *hash_table)
-{
+__global__ void init_hash_table(int n, hash_table_t* hash_table) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
     for (int i = index; i < n; i += stride) {
@@ -33,26 +14,21 @@ void init_hash_table(int n, hash_table_t *hash_table)
     }
 }
 
-__device__
-inline uint32_t fnv1a(char oneByte, uint32_t hash = SEED)
-{
+__device__ inline uint32_t fnv1a(char oneByte, uint32_t hash = SEED) {
     return (oneByte ^ hash) * PRIME;
 }
 
-__device__
-uint32_t hash_function(char *key, uint32_t hash = SEED) //fnv hash
+__device__ uint32_t hash_function(char* key, uint32_t hash = SEED)  // fnv hash
 {
     const char* ptr = (const char*)key;
     for (int i = 0; i < KEY_SIZE; i++) {
         hash = fnv1a(*ptr++, hash);
     }
-    return hash % N;
+    return hash % NUM_ELEMENTS;
 }
 
-__device__
-bool device_memcmp(char* word1, char* word2, uint len)
-{
-    for (uint i=0; i < len; i++) {
+__device__ bool device_memcmp(char* word1, char* word2, uint len) {
+    for (uint i = 0; i < len; i++) {
         if (word1[i] != word2[i]) {
             return false;
         }
@@ -60,12 +36,83 @@ bool device_memcmp(char* word1, char* word2, uint len)
     return true;
 }
 
-__global__
-void insert_entry(hash_table_t *hash_table, char *key, char *word)
-{
+__device__ void HashTable::insert_entry(HashEntry* user_entry) {
+    char* key = user_entry->key;
+    char* word = user_entry->word;
+
     uint32_t hash_val = hash_function(key);
     printf("insert hash_val: %u\n", hash_val);
-    hash_entry_t *entry;
+
+    HashEntryInternal* entry = &entries[hash_val];
+    while (entry->occupied) {
+        // If the keys match, just overwrite the data
+        if (device_memcmp(key, entry->key, KEY_SIZE)) {
+            break;
+        }
+        hash_val = (hash_val + 1) % NUM_ELEMENTS;
+        entry = &entries[hash_val];
+        printf("insert hash_val: %u\n", hash_val);
+    }
+
+    std::memcpy(entry->key, key, KEY_SIZE);
+    std::memcpy(entry->word, word, WORD_SIZE);
+    entry->occupied = true;
+}
+
+__device__ void HashTable::find_entry(HashEntry* user_entry) {
+    char* key = user_entry->key;
+
+    uint32_t hash_val = hash_function(key);
+    printf("get hash_val: %u\n", hash_val);
+
+    HashEntryInternal* entry = &entries[hash_val];
+    // Loop until we reach an empty entry OR find the key
+    while (entry->occupied) {
+        printf("Comparing keys %s & %s\n", entry->key, key);
+        if (device_memcmp(key, entry->key, KEY_SIZE)) {
+            printf("Found word: %s\n", entry->word);
+            std::memcpy(user_entry->word, entry->word, WORD_SIZE);
+            return;
+        }
+        hash_val = (hash_val + 1) % NUM_ELEMENTS;
+        entry = &entries[hash_val];
+        printf("get hash_val: %u\n", hash_val);
+    }
+
+    // key not found, make sure the word we send back is empty
+    user_entry->word[0] = 0;
+    return;
+}
+
+__device__ HashEntryInternal* HashTable::get_entry(uint32_t index) {
+    return &entries[index];
+}
+
+__device__ void init_hash_entry(HashEntryInternal* entry) {
+    std::memset(entry, 0, sizeof(HashEntryInternal));
+}
+
+__global__ void init_hash_table(HashTable* hash_table, int n) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (uint32_t i = index; i < n; i += stride) {
+        HashEntryInternal* entry = hash_table->get_entry(i);
+        init_hash_entry(entry);
+    }
+}
+
+__global__ void hash_insert(HashTable* hash_table, HashEntry* entry) {
+    hash_table->insert_entry(entry);
+}
+
+__global__ void hash_find(HashTable* hash_table, HashEntry* entry) {
+    hash_table->find_entry(entry);
+}
+
+__global__ void insert_entry(hash_table_t* hash_table, char* key, char* word) {
+    uint32_t hash_val = hash_function(key);
+    printf("insert hash_val: %u\n", hash_val);
+    hash_entry_t* entry;
 
     entry = &hash_table->entries[hash_val];
     while (entry->occupied) {
@@ -73,7 +120,7 @@ void insert_entry(hash_table_t *hash_table, char *key, char *word)
         if (device_memcmp(key, entry->key, KEY_SIZE)) {
             break;
         }
-        hash_val =  (hash_val + 1) % N;
+        hash_val = (hash_val + 1) % NUM_ELEMENTS;
         entry = &hash_table->entries[hash_val];
         printf("insert hash_val: %u\n", hash_val);
     }
@@ -82,12 +129,10 @@ void insert_entry(hash_table_t *hash_table, char *key, char *word)
     entry->occupied = true;
 }
 
-__global__
-void get_entry(hash_table_t *hash_table, char *key, char *host_word)
-{
+__global__ void get_entry(hash_table_t* hash_table, char* key, char* host_word) {
     uint32_t hash_val = hash_function(key);
     printf("get hash_val: %u\n", hash_val);
-    hash_entry_t *entry;
+    hash_entry_t* entry;
 
     entry = &hash_table->entries[hash_val];
     // Loop until we reach an empty entry OR find the key
@@ -98,7 +143,7 @@ void get_entry(hash_table_t *hash_table, char *key, char *host_word)
             memcpy(host_word, entry->word, WORD_SIZE);
             return;
         }
-        hash_val =  (hash_val + 1) % N;
+        hash_val = (hash_val + 1) % NUM_ELEMENTS;
         entry = &hash_table->entries[hash_val];
         printf("get hash_val: %u\n", hash_val);
     }
@@ -106,17 +151,14 @@ void get_entry(hash_table_t *hash_table, char *key, char *host_word)
     return;
 }
 
-int main(void)
-{
-    // char *hash_table;
-    hash_table_t *hash_table;
+void test_old_table() {
+    hash_table_t* hash_table;
 
-    cudaMallocManaged(&hash_table, N*sizeof(hash_table));
+    cudaMallocManaged(&hash_table, sizeof(hash_table_t));
 
-    init_hash_table<<<NUM_BLOCKS, BLOCK_SIZE>>>(N, hash_table);
+    init_hash_table<<<NUM_BLOCKS, BLOCK_SIZE>>>(NUM_ELEMENTS, hash_table);
     cudaDeviceSynchronize();
 
-    // TODO run a routine here to test the hash table
     char *key, *word;
     cudaMallocManaged(&key, KEY_SIZE);
     cudaMallocManaged(&word, WORD_SIZE);
@@ -126,13 +168,12 @@ int main(void)
     insert_entry<<<1, 1>>>(hash_table, key, word);
     cudaDeviceSynchronize();
 
-    char *buffer;
+    char* buffer;
     cudaMallocManaged(&buffer, WORD_SIZE);
     get_entry<<<1, 1>>>(hash_table, key, buffer);
     cudaDeviceSynchronize();
     cudaError_t error = cudaGetLastError();
-    if(error != cudaSuccess)
-    {
+    if (error != cudaSuccess) {
         // print the CUDA error message and exit
         printf("CUDA error: %s\n", cudaGetErrorString(error));
         exit(-1);
@@ -147,8 +188,43 @@ int main(void)
         std::cout << "Buffer: " << buffer << std::endl;
     }
 
-    // Free memory
     cudaFree(hash_table);
+    cudaFree(key);
+    cudaFree(word);
+    cudaFree(buffer);
+}
+
+void test_new_table() {
+    HashTable* h =  new HashTable;
+    cudaDeviceSynchronize();
+
+    init_hash_table<<<NUM_BLOCKS, BLOCK_SIZE>>>(h, NUM_ELEMENTS);
+    cudaDeviceSynchronize();
+
+    char test_key[] = "abcdefghijklmnopqrstuvwxyz12345";
+    char test_word[] = "abcdefghijklmnopqrstuvwxyz12345abcdefghijklmnopqrstuvwxyz123456";
+
+    HashEntry* in = new HashEntry(test_key, test_word);
+    HashEntry* out = new HashEntry(test_key);
+    cudaDeviceSynchronize();
+
+    hash_insert<<<1, 1>>>(h, in);
+    cudaDeviceSynchronize();
+    hash_find<<<1, 1>>>(h, out);
+    cudaDeviceSynchronize();
+    if (*in == *out) {
+        std::cout << "Hooray they're equal" << std::endl;
+    } else {
+        std::cout << "Boo I suck" << std::endl;
+    }
+
+    delete h;
+    delete in;
+    delete out;
+}
+
+int main(void) {
+    test_new_table();
 
     return 0;
 }
--- src/hash.h
+++ src/hash.h
@@ -1,19 +1,29 @@
 #ifdef __clang__
+// Added for YouCompleteMe
 #include <__clang_cuda_builtin_vars.h>
-int cudaConfigureCall(dim3 grid_size, dim3 block_size, unsigned shared_size = 0,
-                      cudaStream_t stream = 0);
+#include <cuda_runtime.h>
+#include <host_defines.h>
+int cudaConfigureCall(dim3 grid_size, dim3 block_size, unsigned shared_size = 0, cudaStream_t stream = 0);
 #endif
 #include <math.h>
+
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
 #include <iostream>
 
-static constexpr uint64_t N = 1<<20; // 1M elements
+// Constants for hash table
+static constexpr uint64_t NUM_ELEMENTS = 1 << 20;  // 1M elements
 static constexpr uint KEY_SIZE = 32;
 static constexpr uint WORD_SIZE = 64;
-static constexpr uint BLOCK_SIZE = 256;
-static constexpr uint NUM_BLOCKS = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-static constexpr uint32_t PRIME = 0x01000193; //   16777619
-static const uint32_t SEED  = 0x811C9DC5; // 2166136261
+// Constants used for kernel invocation
+static constexpr uint BLOCK_SIZE = 256;
+static constexpr uint NUM_BLOCKS = (NUM_ELEMENTS + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+// Constants for hash function
+static constexpr uint32_t PRIME = 0x01000193;  //   16777619
+static const uint32_t SEED = 0x811C9DC5;       // 2166136261
 
 struct hash_entry_t {
     bool occupied;
@@ -22,5 +32,82 @@ struct hash_entry_t {
 };
 
 struct hash_table_t {
-    hash_entry_t entries[N];
+    hash_entry_t entries[NUM_ELEMENTS];
+};
+
+class CudaManagedMemory {
+  public:
+    void* operator new(size_t len) {
+        void* ptr;
+        cudaMallocManaged(&ptr, len);
+        return ptr;
+    }
+
+    void operator delete(void* ptr) {
+        cudaFree(ptr);
+    }
+};
+
+class CudaMemory {
+  public:
+    void* operator new(size_t len) {
+        void* ptr;
+        cudaMalloc(&ptr, len);
+        return ptr;
+    }
+
+    void operator delete(void* ptr) {
+        cudaFree(ptr);
+    }
+};
+
+class HashEntry : public CudaManagedMemory {
+  public:
+    HashEntry(char* k, char* w) {
+        std::memset(key, 0, sizeof(key));
+        std::memset(word, 0, sizeof(word));
+
+        std::strcpy(key, k);
+        std::strcpy(word, w);
+    }
+
+    HashEntry(char* k) {
+        std::memset(key, 0, sizeof(key));
+        std::memset(word, 0, sizeof(word));
+
+        std::strcpy(key, k);
+    }
+
+    HashEntry(HashEntry& h) = delete;
+    HashEntry(HashEntry&& h) = delete;
+
+    char key[KEY_SIZE];
+    char word[WORD_SIZE];
+};
+inline bool operator==(const HashEntry& a, const HashEntry& b) {
+    return (std::memcmp(a.key, b.key, KEY_SIZE) == 0) && (std::memcmp(a.word, b.word, WORD_SIZE) == 0);
+}
+
+// Stores extra fields needed internally
+class HashEntryInternal {
+  public:
+    bool occupied;
+    char key[KEY_SIZE];
+    char word[WORD_SIZE];
+};
+
+class HashTable : public CudaMemory {
+  public:
+    // Inserts the entry into the hash table
+    __device__ void insert_entry(HashEntry* user_entry);
+
+    // Finds the entry in the hash table. The word in the user_entry is set to the word found.
+    __device__ void find_entry(HashEntry* user_entry);
+
+    // Returns the pointer to the entry at the given index
+    __device__ HashEntryInternal* get_entry(uint32_t index);
+
+    HashEntryInternal entries[NUM_ELEMENTS];
+
+  private:
 };
