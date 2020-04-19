d21fe6710f3cc2b2c10d15bb00e40af6f98f2008
Shalin Patel <shalinpatel75@gmail.com>
2020-04-19 17:37:30 -0400

    Creating working batch interface for hash table with hardcoded tests

--- src/hash.cu
+++ src/hash.cu
@@ -1,3 +1,5 @@
+#include <ios>
+
 #include "hash.h"
 
 __device__ void init_hash_entry(hash_entry_t* entry) {
@@ -36,14 +38,58 @@ __device__ bool device_memcmp(char* word1, char* word2, uint len) {
     return true;
 }
 
+__global__ void insert_entry(hash_table_t* hash_table, char* key, char* word) {
+    uint32_t hash_val = hash_function(key);
+    printf("insert hash_val: %u\n", hash_val);
+    hash_entry_t* entry;
+
+    entry = &hash_table->entries[hash_val];
+    while (entry->occupied) {
+        // If the keys match, just overwrite the data
+        if (device_memcmp(key, entry->key, KEY_SIZE)) {
+            break;
+        }
+        hash_val = (hash_val + 1) % NUM_ELEMENTS;
+        entry = &hash_table->entries[hash_val];
+        printf("insert hash_val: %u\n", hash_val);
+    }
+    memcpy(entry->key, key, KEY_SIZE);
+    memcpy(entry->word, word, WORD_SIZE);
+    entry->occupied = true;
+}
+
+__global__ void get_entry(hash_table_t* hash_table, char* key, char* host_word) {
+    uint32_t hash_val = hash_function(key);
+    printf("get hash_val: %u\n", hash_val);
+    hash_entry_t* entry;
+
+    entry = &hash_table->entries[hash_val];
+    // Loop until we reach an empty entry OR find the key
+    while (entry->occupied) {
+        printf("Comparing keys %s & %s\n", entry->key, key);
+        if (device_memcmp(key, entry->key, KEY_SIZE)) {
+            printf("Found word: %s\n", entry->word);
+            memcpy(host_word, entry->word, WORD_SIZE);
+            return;
+        }
+        hash_val = (hash_val + 1) % NUM_ELEMENTS;
+        entry = &hash_table->entries[hash_val];
+        printf("get hash_val: %u\n", hash_val);
+    }
+    host_word = nullptr;
+    return;
+}
+
 __device__ void HashTable::insert_entry(HashEntry* user_entry) {
     char* key = user_entry->key;
     char* word = user_entry->word;
 
     uint32_t hash_val = hash_function(key);
     printf("insert hash_val: %u\n", hash_val);
+    printf("Request to insert element with key %s, word %s\n", user_entry->key, user_entry->word);
 
     HashEntryInternal* entry = &entries[hash_val];
+    // TODO handle full case
     while (entry->occupied) {
         // If the keys match, just overwrite the data
         if (device_memcmp(key, entry->key, KEY_SIZE)) {
@@ -57,6 +103,7 @@ __device__ void HashTable::insert_entry(HashEntry* user_entry) {
     std::memcpy(entry->key, key, KEY_SIZE);
     std::memcpy(entry->word, word, WORD_SIZE);
     entry->occupied = true;
+    printf("Inserted element at hash_val %u, key %s, word %s\n", hash_val, entry->key, entry->word);
 }
 
 __device__ void HashTable::find_entry(HashEntry* user_entry) {
@@ -109,46 +156,39 @@ __global__ void hash_find(HashTable* hash_table, HashEntry* entry) {
     hash_table->find_entry(entry);
 }
 
-__global__ void insert_entry(hash_table_t* hash_table, char* key, char* word) {
-    uint32_t hash_val = hash_function(key);
-    printf("insert hash_val: %u\n", hash_val);
-    hash_entry_t* entry;
-
-    entry = &hash_table->entries[hash_val];
-    while (entry->occupied) {
-        // If the keys match, just overwrite the data
-        if (device_memcmp(key, entry->key, KEY_SIZE)) {
-            break;
-        }
-        hash_val = (hash_val + 1) % NUM_ELEMENTS;
-        entry = &hash_table->entries[hash_val];
-        printf("insert hash_val: %u\n", hash_val);
+__global__ void hash_insert_batch(HashTable* hash_table, HashEntryBatch *entry_batch, uint num_entries) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (uint32_t i = index; i < num_entries; i += stride) {
+        printf("Batch insert index %u\n", i);
+        hash_table->insert_entry(&entry_batch->entries[i]);
     }
-    memcpy(entry->key, key, KEY_SIZE);
-    memcpy(entry->word, word, WORD_SIZE);
-    entry->occupied = true;
 }
 
-__global__ void get_entry(hash_table_t* hash_table, char* key, char* host_word) {
-    uint32_t hash_val = hash_function(key);
-    printf("get hash_val: %u\n", hash_val);
-    hash_entry_t* entry;
-
-    entry = &hash_table->entries[hash_val];
-    // Loop until we reach an empty entry OR find the key
-    while (entry->occupied) {
-        printf("Comparing keys %s & %s\n", entry->key, key);
-        if (device_memcmp(key, entry->key, KEY_SIZE)) {
-            printf("Found word: %s\n", entry->word);
-            memcpy(host_word, entry->word, WORD_SIZE);
-            return;
-        }
-        hash_val = (hash_val + 1) % NUM_ELEMENTS;
-        entry = &hash_table->entries[hash_val];
-        printf("get hash_val: %u\n", hash_val);
+__global__ void hash_find_batch(HashTable* hash_table, HashEntryBatch* entry_batch, uint num_entries) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (uint32_t i = index; i < num_entries; i += stride) {
+        hash_table->find_entry(&entry_batch->entries[i]);
     }
-    host_word = nullptr;
-    return;
+}
+
+// Used for debugging only
+__global__ void hash_print_all_entries(HashTable* hash_table, int n) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (uint32_t i = index; i < n; i += stride) {
+        HashEntryInternal* entry = hash_table->get_entry(i);
+        if (entry->occupied) {
+            printf("%u: (%s, %s)\n", i, entry->key, entry->word);
+        }
+    }
+}
+void print_all_entries(HashTable* hash_table, int n) {
+    printf("_____ ALL ENTRIES _____\n");
+    hash_print_all_entries<<<NUM_BLOCKS, BLOCK_SIZE>>>(hash_table, n);
+    cudaDeviceSynchronize();
+    printf("_______________________\n");
 }
 
 void test_old_table() {
@@ -195,7 +235,7 @@ void test_old_table() {
 }
 
 void test_new_table() {
-    HashTable* h =  new HashTable;
+    HashTable* h = new HashTable;
     cudaDeviceSynchronize();
 
     init_hash_table<<<NUM_BLOCKS, BLOCK_SIZE>>>(h, NUM_ELEMENTS);
@@ -204,23 +244,64 @@ void test_new_table() {
     char test_key[] = "abcdefghijklmnopqrstuvwxyz12345";
     char test_word[] = "abcdefghijklmnopqrstuvwxyz12345abcdefghijklmnopqrstuvwxyz123456";
 
-    HashEntry* in = new HashEntry(test_key, test_word);
-    HashEntry* out = new HashEntry(test_key);
+    char test_key2[] = "shalin";
+    char test_word2[] = "wuzhere";
+
+    uint num_elems = 2;
+
+    HashEntryBatch* in_batch = new HashEntryBatch;
+    in_batch->at(0).set(test_key, test_word);
+    in_batch->at(1).set(test_key2, test_word2);
+
+    HashEntryBatch* out_batch = new HashEntryBatch;
+    out_batch->at(0).set(test_key);
+    out_batch->at(1).set(test_key2);
     cudaDeviceSynchronize();
 
-    hash_insert<<<1, 1>>>(h, in);
+    /*for (uint i = 0; i < num_elems; i++) {*/
+        /*printf("ins[%u] has key %s word %s\n", i, in_batch.at[i]->key, in_batch[i]->word);*/
+        /*printf("outs[%u] has key %s word %s\n", i, out_batch[i].key, out_batch[i].word);*/
+    /*}*/
+    cudaCheckErrors();
+    hash_insert_batch<<<2, 1>>>(h, in_batch, num_elems);
+    // hash_insert<<<1, 1>>>(h, ins[0]);
+    // hash_insert<<<1, 1>>>(h, ins[1]);
     cudaDeviceSynchronize();
-    hash_find<<<1, 1>>>(h, out);
+    cudaCheckErrors();
+
+    // hash_find_batch<<<2, 1>>>(h, outs[0], num_elems);
+    hash_find<<<1, 1>>>(h, &out_batch->entries[0]);
     cudaDeviceSynchronize();
-    if (*in == *out) {
-        std::cout << "Hooray they're equal" << std::endl;
-    } else {
-        std::cout << "Boo I suck" << std::endl;
-    }
+    cudaCheckErrors();
+
+    hash_find<<<1, 1>>>(h, &out_batch->entries[1]);
+    cudaDeviceSynchronize();
+    cudaCheckErrors();
+
+    checkEntryEqual(in_batch->at(0), out_batch->at(0));
+    checkEntryEqual(in_batch->at(1), out_batch->at(1));
+
+    print_all_entries(h, NUM_ELEMENTS);
+
+    /*if (*in == *out) {*/
+    /*std::cout << "Hooray they're equal" << std::endl;*/
+    /*} else {*/
+    /*std::cout << "Boo I suck" << std::endl;*/
+    /*}*/
+
+    /*hash_insert<<<1, 1>>>(h, in);*/
+    /*cudaDeviceSynchronize();*/
+    /*hash_find<<<1, 1>>>(h, out);*/
+    /*cudaDeviceSynchronize();*/
+    /*if (*in == *out) {*/
+    /*std::cout << "Hooray they're equal" << std::endl;*/
+    /*} else {*/
+    /*std::cout << "Boo I suck" << std::endl;*/
+    /*}*/
 
     delete h;
-    delete in;
-    delete out;
+    delete in_batch;
+    delete out_batch;
 }
 
 int main(void) {
--- src/hash.h
+++ src/hash.h
@@ -16,6 +16,7 @@ int cudaConfigureCall(dim3 grid_size, dim3 block_size, unsigned shared_size = 0,
 static constexpr uint64_t NUM_ELEMENTS = 1 << 20;  // 1M elements
 static constexpr uint KEY_SIZE = 32;
 static constexpr uint WORD_SIZE = 64;
+static constexpr uint BATCH_SIZE = 2;
 
 // Constants used for kernel invocation
 static constexpr uint BLOCK_SIZE = 256;
@@ -63,23 +64,30 @@ class CudaMemory {
 
 class HashEntry : public CudaManagedMemory {
   public:
-    HashEntry(char* k, char* w) {
+    HashEntry() {
         std::memset(key, 0, sizeof(key));
         std::memset(word, 0, sizeof(word));
-
-        std::strcpy(key, k);
-        std::strcpy(word, w);
     }
 
-    HashEntry(char* k) {
-        std::memset(key, 0, sizeof(key));
-        std::memset(word, 0, sizeof(word));
-
+    HashEntry(char* k) : HashEntry() {
         std::strcpy(key, k);
+    }
+
+    HashEntry(char* k, char* w) : HashEntry(k) {
+        std::strcpy(word, w);
     }
 
     HashEntry(HashEntry& h) = delete;
     HashEntry(HashEntry&& h) = delete;
+
+    void set(char* k) {
+        std::strcpy(key, k);
+    }
+
+    void set(char* k, char* w) {
+        std::strcpy(key, k);
+        std::strcpy(word, w);
+    }
 
     char key[KEY_SIZE];
     char word[WORD_SIZE];
@@ -87,7 +95,27 @@ class HashEntry : public CudaManagedMemory {
 inline bool operator==(const HashEntry& a, const HashEntry& b) {
     return (std::memcmp(a.key, b.key, KEY_SIZE) == 0) && (std::memcmp(a.word, b.word, WORD_SIZE) == 0);
 }
+inline std::ostream& operator<<(std::ostream& outs, const HashEntry& entry) {
+    return outs << "(" << entry.key << ", " << entry.word << ")";
+}
 
+class HashEntryBatch : public CudaManagedMemory {
+  public:
+    HashEntryBatch() {
+        memset(entries, 0, sizeof(entries));
+    }
+    HashEntryBatch(HashEntryBatch& h) = delete;
+    HashEntryBatch(HashEntryBatch&& h) = delete;
+
+    HashEntry entries[BATCH_SIZE];
+
+    // Returns the entry at the given index
+    inline HashEntry& at(std::size_t i) {
+        return entries[i];
+    }
+};
+
+// TODO should this just inherit from HashEntry?
 // Stores extra fields needed internally
 class HashEntryInternal {
   public:
@@ -111,3 +139,40 @@ class HashTable : public CudaMemory {
 
   private:
 };
+
+void checkEntryEqual(HashEntry& in, HashEntry& out) {
+    if (in == out) {
+        std::cout << "Entries " << in << " and " << out << " are equal" << std::endl;
+    } else {
+        std::cout << "Entries " << in << " and " << out << " are NOT equal!" << std::endl;
+    }
+}
+
+// ----------------------------------------------
+// Debugging Stuff Below
+// ----------------------------------------------
+#include <execinfo.h>
+void print_trace(void) {
+    char **strings;
+    size_t i, size;
+    enum Constexpr { MAX_SIZE = 1024 };
+    void *array[MAX_SIZE];
+    size = backtrace(array, MAX_SIZE);
+    strings = backtrace_symbols(array, size);
+    for (i = 0; i < size; i++)
+        printf("%s\n", strings[i]);
+    puts("");
+    free(strings);
+}
+
+#define cudaCheckErrors() { cudaCheckErrorsFn(__FILE__, __LINE__); }
+
+inline void cudaCheckErrorsFn(const char *file, int line) {
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        // print the CUDA error message and exit
+        printf("CUDA error: %s %s %d\n", cudaGetErrorString(error), file, line);
+        print_trace();
+        exit(-1);
+    }
+}
