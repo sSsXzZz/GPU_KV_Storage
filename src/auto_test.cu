7d15669f2f5b0dae451504b427746878cccc122d
Shalin Patel <shalinpatel75@gmail.com>
2020-08-02 15:54:32 -0400

    Created Testing classes to setup functionality to test tables multiple times
    Note: new testing framework has use of cuda pinned memory rolled in

--- src/auto_test.cu
+++ src/auto_test.cu
@@ -5,7 +5,6 @@
 #include <unordered_map>
 
 #include "cuda_profiler_api.h"
-
 #include "hash.h"
 #include "utils.h"
 
@@ -24,6 +23,229 @@ using DataMap = std::unordered_map<std::string, std::string>;
 
 static constexpr char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 static constexpr uint NUM_TEST_ENTRIES = 1000000;
+
+class HashTableTestBase {
+  public:
+    HashTableTestBase(std::string name) : name_{name} {
+    }
+
+    virtual ~HashTableTestBase(){};
+
+    // Inserts every entry in the given DataMap
+    virtual void insert_all(DataMap& test_data) = 0;
+
+    // Finds every entry in the given DataMap
+    virtual void find_all(DataMap& test_data, bool check_data) = 0;
+
+    void test_all(DataMap& test_data, bool check_data) {
+        time_t t0_insert = get_time_us();
+        insert_all(test_data);
+        time_t t1_insert = get_time_us();
+        insert_all_times.emplace_back(t1_insert - t0_insert);
+        std::cout << name_ << " Insert time: " << t1_insert - t0_insert << " us" << std::endl;
+
+        time_t t0_find = get_time_us();
+        find_all(test_data, check_data);
+        time_t t1_find = get_time_us();
+        find_all_times.emplace_back(t1_find - t0_find);
+        std::cout << name_ << " Find time: " << t1_find - t0_find << " us" << std::endl;
+    }
+
+    void print_averages() {
+        time_t insert_all_avg =
+            std::accumulate(insert_all_times.begin(), insert_all_times.end(), 0) / insert_all_times.size();
+        time_t find_all_avg = std::accumulate(find_all_times.begin(), find_all_times.end(), 0) / find_all_times.size();
+
+        std::cout << name_ << " Avg Insert time: " << insert_all_avg << " us" << std::endl;
+        std::cout << name_ << " Avg Find time: " << find_all_avg << " us" << std::endl;
+    }
+
+  protected:
+    std::string name_;
+    std::vector<time_t> insert_all_times;
+    std::vector<time_t> find_all_times;
+};
+
+class CpuHashTableTest : public HashTableTestBase {
+  public:
+    CpuHashTableTest(std::string name) : HashTableTestBase(name) {
+        h = new CpuHashTable();
+        h->init();
+
+        in_batch = new CpuHashEntryBatch;
+        out_batch = new CpuHashEntryBatch;
+
+        in_entry = new CpuHashEntry;
+        out_entry = new CpuHashEntry;
+    }
+
+    ~CpuHashTableTest() {
+        delete h;
+        delete in_batch;
+        delete out_batch;
+    }
+
+  protected:
+    void insert_all(DataMap& test_data) override {
+        for (auto& entry : test_data) {
+            const std::string& key = entry.first;
+            const std::string& word = entry.second;
+
+            std::memcpy(&in_entry->key, key.c_str(), key.size());
+            std::memcpy(&in_entry->word, word.c_str(), word.size());
+
+            if (in_entry->key[0] == '\0') {
+                std::cout << "found the empty key\n";
+            }
+            h->insert_entry(in_entry);
+        }
+    }
+
+    void compare_data(DataMap& test_data, CpuHashEntry* out_entry) {
+        const char* out_key = out_entry->key;
+        const char* out_word = out_entry->word;
+
+        std::string key(out_key, KEY_SIZE);
+        std::string word = test_data[key];
+        if (strcmp(key.c_str(), out_key) == 0 && strcmp(word.c_str(), out_word) == 0) {
+            // printf("entry(%s, %s) == map_entry(%s, %s)\n", key.c_str(), word.c_str(), out_key, out_word);
+        } else {
+            printf("CPU: entry(%s, %s) != map_entry(%s, %s)\n", key.c_str(), word.c_str(), out_key, out_word);
+            abort_with_trace();
+        }
+    }
+
+    void find_all(DataMap& test_data, bool check_data) override {
+        for (auto& entry : test_data) {
+            const std::string& key = entry.first;
+
+            std::memcpy(&out_entry->key, key.c_str(), key.size());
+            std::memset(&out_entry->word, 0, WORD_SIZE);
+
+            h->find_entry(out_entry);
+            if (check_data) {
+                compare_data(test_data, out_entry);
+            }
+        }
+    }
+
+    CpuHashTable* h;
+    CpuHashEntryBatch* in_batch;
+    CpuHashEntryBatch* out_batch;
+    CpuHashEntry* in_entry;
+    CpuHashEntry* out_entry;
+};
+
+class HybridHashTableTest : public HashTableTestBase {
+  public:
+    HybridHashTableTest(std::string name) : HashTableTestBase(name) {
+        h = new HybridHashTable;
+        cudaCheckErrors();
+
+        for (uint i = 0; i < NUM_BATCHES; i++) {
+            cudaMallocHost(&in_batches[i], sizeof(HybridHashEntryBatch));
+        }
+        cudaMallocHost(&out_batch, sizeof(HybridHashEntryBatch));
+        cudaDeviceSynchronize();
+        cudaCheckErrors();
+    }
+
+    ~HybridHashTableTest() {
+        delete h;
+        for (uint i = 0; i < NUM_BATCHES; i++) {
+            cudaFreeHost(&in_batches[i]);
+        }
+        cudaFreeHost(&out_batch);
+    }
+
+  protected:
+    void insert_all(DataMap& test_data) override {
+        uint in_batch_index = 0;
+        HybridHashEntryBatch* in_batch = in_batches[in_batch_index];
+
+        uint batch_index = 0;  // Count of entries in this batch
+        for (auto& entry : test_data) {
+            const std::string& key = entry.first;
+            const std::string& word = entry.second;
+            std::memcpy(&in_batch->keys[batch_index], key.c_str(), key.size());
+            std::memcpy(&in_batch->words[batch_index], word.c_str(), word.size());
+            batch_index++;
+
+            // Insert entries using max BATCH_SIZE
+            if (batch_index == BATCH_SIZE) {
+                h->insert_batch(in_batch, BATCH_SIZE);
+                in_batch_index++;
+                in_batch = in_batches[in_batch_index];
+                // cudaCheckErrors();
+                batch_index = 0;
+            }
+        }
+        // Insert remaining entries
+        if (batch_index > 0 && batch_index < BATCH_SIZE) {
+            h->insert_batch(in_batch, batch_index);
+            // cudaCheckErrors();
+        }
+    }
+
+    void compare_data(DataMap& test_data, HybridHashEntryBatch* out_batch, uint num_entries) {
+        for (uint i = 0; i < num_entries; i++) {
+            const char* out_key = out_batch->keys[i];
+            const char* out_word = out_batch->words[i];
+
+            std::string key(out_key, KEY_SIZE);
+            auto it = test_data.find(key);
+            if (it == test_data.end()) {
+                printf("Key %s not found in the test data map\n", key.c_str());
+                abort_with_trace();
+            }
+            std::string word = test_data[key];
+
+            if (strcmp(key.c_str(), out_key) == 0 && strcmp(word.c_str(), out_word) == 0) {
+                // printf("entry(%s, %s) == map_entry(%s, %s)\n", key.c_str(), word.c_str(), out_key, out_word);
+            } else {
+                printf("GPU: entry(%s, %s) != map_entry(%s, %s)\n", key.c_str(), word.c_str(), out_key, out_word);
+                abort_with_trace();
+            }
+        }
+    }
+
+    void find_all(DataMap& test_data, bool check_data) override {
+        uint batch_index = 0;
+        for (auto& entry : test_data) {
+            const std::string& key = entry.first;
+            std::memcpy(&out_batch->keys[batch_index], key.c_str(), key.size());
+            std::memset(&out_batch->words[batch_index], 0, WORD_SIZE);
+            batch_index++;
+
+            // Insert entries using max BATCH_SIZE
+            if (batch_index == BATCH_SIZE) {
+                h->find_batch(out_batch, BATCH_SIZE);
+
+                if (check_data) {
+                    cudaCheckErrors();
+                    compare_data(test_data, out_batch, BATCH_SIZE);
+                }
+                batch_index = 0;
+            }
+        }
+        // Check remaining entries
+        if (batch_index > 0 && batch_index < BATCH_SIZE) {
+            h->find_batch(out_batch, batch_index);
+
+            if (check_data) {
+                cudaCheckErrors();
+                compare_data(test_data, out_batch, batch_index);
+            }
+        }
+    }
+
+  protected:
+    static constexpr uint NUM_BATCHES = (NUM_TEST_ENTRIES / BATCH_SIZE) + (NUM_TEST_ENTRIES % BATCH_SIZE != 0 ? 1 : 0);
+
+    HybridHashTable* h;
+    HybridHashEntryBatch* in_batches[NUM_BATCHES];
+    HybridHashEntryBatch* out_batch;
+};
 
 void checkEntryEqual(GpuHashEntry& in, GpuHashEntry& out) {
     if (in == out) {
@@ -272,8 +494,12 @@ int main(void) {
         test_entries[key] = word;
     }
 
-    test_hybrid_table(test_entries);
-    test_cpu_table(test_entries);
+    /*test_hybrid_table(test_entries);*/
+    //    test_cpu_table(test_entries);
+    HybridHashTableTest hybrid_tester("Hybrid");
+    hybrid_tester.test_all(test_entries, false);
+    CpuHashTableTest cpu_tester("CPU");
+    cpu_tester.test_all(test_entries, false);
 
     return 0;
 }
